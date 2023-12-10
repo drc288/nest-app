ebf51ed52ed81f1d376c68ac8051edaeb6f0695a
David Rosero Calle <davidroserocalle@gmail.com>
2023-12-10 13:42:53 -0500

    Add env process and load in main

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as morgan from 'morgan';
+import { CORS_OPTIONS } from './constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  // Set logger middleware
+  app.use(morgan('short'));
+  app.setGlobalPrefix('api');
+  app.enableCors(CORS_OPTIONS);
+  await app.listen(configService.get('PORT'));
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
